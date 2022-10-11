4b49273cffad1a7ec9cc5486f2fbb2e1fc04141d
KopylovaRimma <112937566+KopylovaRimma@users.noreply.github.com>
2022-10-11 12:39:21 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 
 // $("h1").fadeOut(1000);
 // $("h1").fadeIn(1000);
+/*
+for (var i = 0; i < 5;i++) {
+    $("h1").slideUp(1000);
+    $("h1").slideDown(1000);
+}
+$("h1").append("<p> Заголовок</p>");*/
 
-// for (var i = 0; i < 5;i++) {
-//     $("h1").slideUp(1000);
-//     $("h1").slideDown(1000);
-// }
-// $("h1").append("<p> Заголовок</p>");
-
-// for(var i = 0; i < 5; i++) {
-//     $("h1").hide(1000);
-//     $("h1").show(1000);
-// }
-// $("h1").append("<p>Заголовок</p>");
+//  for(var i = 0; i < 5; i++) {
+//      $("h1").hide(1000);
+//      $("h1").show(1000);
+//  }
+//  $("h1").append("<p>Заголовок</p>");
 
 // for(var i =0; i<5; i++) {
 //     var friend = prompt("введите имя ваших друзей");
@@ -152,7 +152,7 @@ var clicks = 0;
 var clickHandler = function() {
     setInterval(moveHeading, speed);
     speed--;
-    $("h1").text("Количество кликов " + clicks);
+    $("h1").text(clicks + " кликов");
     clicks++;
     if(clicks>=10){
         clearInterval(a);
